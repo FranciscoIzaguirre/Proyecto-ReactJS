ae8535b67da13251d77d44932ebf9c70bad3cf0e
FranciscoIzaguirre <franizaguirre0804@gmail.com>
2023-11-21 13:57:39 -0300

    prueba 1

--- Mercado de Bebidas/src/App.jsx
+++ Mercado de Bebidas/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import ItemCount from './components/ItemCount/ItemCount'
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
    <div>
     <NavBar />
     <ItemListContainer greeting={'Bienvenidos al Mercado De Bebidas'}/>
+    <ItemCount initial={1} stock={10} onAdd={(quantity)  => console.log('Cantidad agregada' , quantity)} />
    </div>
   )
 }
